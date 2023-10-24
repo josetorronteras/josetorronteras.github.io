0025dee3c4a1b5e7c6c9e75926b3f15cfd142b69
Jose Torronteras <jose.torronteras@bcncgroup.com>
2023-10-25 00:14:11 +0200

    💿 Project first data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,23 +5,6 @@ export type Project = {
   isComingSoon?: boolean;
 };
 
-const projects: Project[] = [
-  {
-    title: "Dictionary App",
-    techs: ["ReactJS (NextJS)", "react-query", "zod"],
-    link: "https://github.com/MaeWolff/dictionary-app",
-  },
-  {
-    title: "Portfolio / Lina BLIDI",
-    techs: ["ReactJS (NextJS)", "TypeScript"],
-    link: "https://www.linablidi.fr/",
-  },
-  {
-    title: "Portfolio / Template",
-    techs: ["Astro"],
-    link: "/",
-    isComingSoon: true,
-  },
-];
+const projects: Project[] = [];
 
 export default projects;
--- src/data/theme.ts
+++ src/data/theme.ts
@@ -3,20 +3,12 @@ import type { TailwindColor } from "@/utils/types/tailwind";
 type Theme = {
   colors: {
     primary: TailwindColor;
-    blur: {
-      top: TailwindColor;
-      bottom: TailwindColor;
-    };
   };
 };
 
 const theme: Theme = {
   colors: {
     primary: "orange",
-    blur: {
-      top: "orange",
-      bottom: "violet",
-    },
   },
 };
 

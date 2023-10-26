31a35cff51a9150d5894c3b04312a28cdd499a96
Jose Torronteras <jose.torronteras@bcncgroup.com>
2023-10-26 18:33:54 +0200

    Add darkmode, add navigation item, remove tailwind themes

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -9,13 +9,13 @@ type Presentation = {
   title: string;
   description: string;
   socials: Social[];
-  profile?: string;
+  profile: string;
 };
 
 const presentation: Presentation = {
   mail: "@gmail.com",
   title: "Hi, I’m Jose Torronteras 👋",
-  // profile: "/profile.webp",
+  profile: "/public/profile.webp",
   description: "I'm an experienced *iOS developer* with a passion for building beautiful, performant and user-friendly applications. I have a deep understanding of *Swift, Objective-C, SwiftUI, UIKit, Core Data*, and other technologies required for iOS app development.",
   socials: [
     {
--- src/utils/removeTrailingSlash.ts
+++ src/utils/removeTrailingSlash.ts
@@ -1,5 +1,3 @@
 export default function removeTrailingSlash(pathname: string) {
-  const matchTrailingSlash = /\w+\/$/;
-  if (matchTrailingSlash.test(pathname)) return pathname.slice(0, -1);
-  return pathname;
+  return pathname === "/" ? pathname : pathname.replace(/\/$/, '');
 }

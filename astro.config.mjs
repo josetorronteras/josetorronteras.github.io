44fb1f23c68692e24c39dec588e47771defac0fd
Jose Torronteras <jose.torronteras@bcncgroup.com>
2023-10-27 13:44:15 +0200

    Add base path and add build step to ci_check.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import { SITE_URL } from "./src/data/config";
 export default defineConfig({
   integrations: [tailwind(), sitemap(), robotsTxt()],
   site: SITE_URL,
+  base: '/josetorronteras.github.io',
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {

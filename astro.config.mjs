beece7214386043b844046589985b922aba7629e
Jose Torronteras <jose.torronteras@bcncgroup.com>
2023-11-01 19:08:13 +0100

    Add Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    robotsTxt(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",

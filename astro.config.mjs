8d1ae861bb95752b8c3face45c14a21e88b0d794
Jose Torronteras <jose.torronteras@bcncgroup.com>
2023-11-04 21:14:17 +0100

    🗑 Remove third packages (#16)
    CHANGELOG
    
    Remove 'astro-seo' and 'astro-robots-txt' packages
    New images
    New head tags
    
    ---------
    
    Co-authored-by: josetorronteras <josetorronteras@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,19 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
-import robotsTxt from "astro-robots-txt";
-import { SITE_URL } from "./src/data/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://josetorronteras.es",
   integrations: [
     tailwind(),
     sitemap(),
-    robotsTxt(),
     partytown({
       config: {
         forward: ["dataLayer.push"],
       },
     }),
   ],
-  site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {

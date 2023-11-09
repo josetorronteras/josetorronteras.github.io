5f9576b8ed8704a706ea6518f121ff3eda2cc0d8
Jose Torronteras <jose.torronteras@bcncgroup.com>
2023-11-09 22:46:37 +0100

    📕 Add project content (#22)
    CHANGELOG
    
    Add projects data
    ---------
    
    Co-authored-by: josetorronteras <josetorronteras@users.noreply.github.com>

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,4 +5,31 @@ export type Project = {
   isComingSoon?: boolean;
 };
 
-export const projects: Project[] = [];
+export const projects: Project[] = [
+  {
+    title: "Tennis Score Counter",
+    techs: ["swiftui", "watchos", "health kit"],
+    link: "https://github.com/josetorronteras/",
+    isComingSoon: true,
+  },
+  {
+    title: "Music genre classification",
+    techs: ["python", "deep learning", "keras"],
+    link: "https://github.com/josetorronteras/Music-Genre-Classification",
+  },
+  {
+    title: "CIFAR10 vision classifier",
+    techs: ["swift", "python", "mlmodel", "vision"],
+    link: "https://github.com/josetorronteras/CIFAR10VisionClassifier",
+  },
+  {
+    title: "Monkey species classification",
+    techs: ["python", "deep learning", "keras"],
+    link: "https://github.com/josetorronteras/Monkey-Species-Classification",
+  },
+  {
+    title: "WeatherApp",
+    techs: ["swift", "ios", "mvvm"],
+    link: "https://github.com/josetorronteras/WeatherApp",
+  },
+];

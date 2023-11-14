ad50c1396c85fe0a0133e273712c01dc49cf4a6b
josetorronteras <josetorronteras@users.noreply.github.com>
2023-11-14 22:13:30 +0000

    Code formatter (Prettier)

--- src/data/careers.ts
+++ src/data/careers.ts
@@ -10,14 +10,14 @@ export type Career = {
 export type Company = {
   name: string;
   link: string;
-}
+};
 
 export type App = {
   name: string;
   link: string;
   description: string;
   img: string;
-}
+};
 
 export const careers: Career[] = [
   {
@@ -25,11 +25,11 @@ export const careers: Career[] = [
     companies: [
       {
         name: "BCNC Group",
-        link: "https://bcncgroup.com/"
+        link: "https://bcncgroup.com/",
       },
       {
         name: "Telefonica",
-        link: "https://www.telefonica.com/es/"
+        link: "https://www.telefonica.com/es/",
       },
     ],
     startDate: "feb. 2023",
@@ -38,16 +38,17 @@ export const careers: Career[] = [
     app: {
       name: "Smart Wifi",
       link: "https://apps.apple.com/es/app/smart-wifi-de-movistar/id1138713745",
-      description: "The Movistar Smart WiFi app empowers users to manage their home connectivity efficiently.It offers control over connected devices, optimization tools, password customization, and security features for worry free browsing.",
-      img: "/assets/apps/logo-smart-wifi.webp"
-    }
+      description:
+        "The Movistar Smart WiFi app empowers users to manage their home connectivity efficiently.It offers control over connected devices, optimization tools, password customization, and security features for worry free browsing.",
+      img: "/assets/apps/logo-smart-wifi.webp",
+    },
   },
   {
     rol: "Senior iOS Developer",
     companies: [
       {
         name: "Sngular",
-        link: "https://www.sngular.com/"
+        link: "https://www.sngular.com/",
       },
     ],
     startDate: "feb. 2022",
@@ -56,20 +57,21 @@ export const careers: Career[] = [
     app: {
       name: "Torcal",
       link: "https://apps.apple.com/es/app/torcal-autoescuelas/id1588217480",
-      description: "The Torcal app revolutionizes driver education, offering flexible theoretical training and easy booking for practical lessons. Master the theory at your pace, access test materials, and get personalized support. Reserve, track progress, and choose instructors seamlessly for practical sessions.",
-      img: "/assets/apps/logo-torcal.webp"
-    }
+      description:
+        "The Torcal app revolutionizes driver education, offering flexible theoretical training and easy booking for practical lessons. Master the theory at your pace, access test materials, and get personalized support. Reserve, track progress, and choose instructors seamlessly for practical sessions.",
+      img: "/assets/apps/logo-torcal.webp",
+    },
   },
   {
     rol: "Analyst iOS developer",
     companies: [
       {
         name: "Alten",
-        link: "https://www.alten.es/"
+        link: "https://www.alten.es/",
       },
       {
         name: "Telefonica",
-        link: "https://www.telefonica.com/es/"
+        link: "https://www.telefonica.com/es/",
       },
     ],
     startDate: "sept. 2020",
@@ -78,16 +80,17 @@ export const careers: Career[] = [
     app: {
       name: "Movistar TV",
       link: "https://apps.apple.com/pe/app/movistar-tv-app-perú/id1054940261",
-      description: "Movistar TV is a mobile application that allows you to watch TV (live and on demand) and movies on your mobile device.",
-      img: "/assets/apps/logo-movistar-tv.webp"
-    }
+      description:
+        "Movistar TV is a mobile application that allows you to watch TV (live and on demand) and movies on your mobile device.",
+      img: "/assets/apps/logo-movistar-tv.webp",
+    },
   },
   {
     rol: "iOS app developer",
     companies: [
       {
         name: "Redsys Servicios de Procesamiento S. L.",
-        link: "http://www.redsys.es/"
+        link: "http://www.redsys.es/",
       },
     ],
     startDate: "jul. 2019",
@@ -96,8 +99,9 @@ export const careers: Career[] = [
     app: {
       name: "Bizum",
       link: "https://bizum.es/",
-      description: "Bizum is a mobile payment service that allows you to send and receive money from your mobile phone. It is a service that is available to all Spanish banks and is free for users.",
-      img: "/assets/apps/logo-bizum.webp"
-    }
-  }
-]
+      description:
+        "Bizum is a mobile payment service that allows you to send and receive money from your mobile phone. It is a service that is available to all Spanish banks and is free for users.",
+      img: "/assets/apps/logo-bizum.webp",
+    },
+  },
+];
--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -14,6 +14,6 @@ export const navigationItems: NavigationItem[] = [
   },
   {
     label: "Career",
-    path: "/career"
-  }
+    path: "/career",
+  },
 ];

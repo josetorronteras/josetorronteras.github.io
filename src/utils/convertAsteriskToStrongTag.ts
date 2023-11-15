7ba20ff83e3fa3c3bac6178bfa1513b3b9d40f0f
josetorronteras <josetorronteras@users.noreply.github.com>
2023-11-15 22:46:52 +0000

    Code formatter (Prettier)

--- src/utils/convertAsteriskToStrongTag.ts
+++ src/utils/convertAsteriskToStrongTag.ts
@@ -1,6 +1,6 @@
 export default function convertAsteriskToStrongTag(str: string) {
   return str.replace(
     /\*{1,2}(.*?)\*{1,2}/g,
-    `<strong class="font-normal text-orange">$1</strong>`
+    `<strong class="font-normal text-orange">$1</strong>`,
   );
 }

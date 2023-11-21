dfca16f20363d431a39e40a3ed75b618e93dd8a1
Jose Torronteras <jose.torronteras@bcncgroup.com>
2023-11-21 23:54:46 +0100

    Sentry integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import sentry from "@sentry/astro";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +13,13 @@ export default defineConfig({
     partytown({
       config: {
         forward: ["dataLayer.push"],
+      },
+    }),
+    sentry({
+      dsn: "https://85fdb807f0dccd1f13b7b865a6c3ebd4@o206307.ingest.sentry.io/4506266192314368",
+      sourceMapsUploadOptions: {
+        project: "josetorronteras",
+        authToken: import.meta.env.SENTRY_AUTH_TOKEN,
       },
     }),
   ],
